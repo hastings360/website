7a05d9c5f2d68cd09fa7a400f744c81c237637fa
Larry <hastings360@gmail.com>
2018-05-13 14:12:53 -0400

    asdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,43 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public constructor(private titleService: Title){}
+  public constructor(private titleService: Title){
+
+
+    const url = "https://services.omegafi.com/omegafi_connect/api/v2/chapters";
+    const userName = "DXConnect";
+    const passWord = "SXJnHm_0";
+    
+    let headers = new Headers();
+    
+    headers.append('Authorization', 'Basic' + btoa(userName + ":" + passWord));
+    headers.append('credentials','include');
+    headers.append('content-type','application/json');
+    
+    console.log(headers.get('credentials'));
+    
+    function GetChapters(){
+        
+        var chapterRequest = new Request(url);
+    
+        fetch(chapterRequest,headers)
+            .then( res => console.log(res))
+            .catch( error => console.log(error))
+    
+        
+    
+        
+        
+        
+    
+    };
+    
+    GetChapters();
+        
+
+
+    
+  }
 
   public setTitle( newTitle: string){
     this.titleService.setTitle(newTitle);
