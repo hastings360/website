969206f6b1dce9ce2bdf306603cb37a31e0f6207
Larry <hastings360@gmail.com>
2018-10-01 10:53:47 -0400

    testing

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-declare var math:any
+declare let math: any;
 
 @Component({
   selector: 'app-calculator',
@@ -12,55 +12,55 @@ export class CalculatorComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit() {
-      var display = '0';
-      //Adds and sorts values to the display variable
-      function addToDisplay(x){						
-        if(display === '0'){
+      let display = '0';
+      // Adds and sorts values to the displaletiable
+      function addToDisplay(x) {
+        if (display === '0') {
           display = x;
-        }else if((display + x).match(/[*\/\+-][*\/\+-]/)){
+        } else if ((display + x).match(/[*\/\+-][*\/\+-]/)) {
           return;
-        }else{
+        } else {
           display = display  + x;
-        };
-        var y = document.getElementById('calc-window');
+        }
+        const y = document.getElementById('calc-window');
         y.innerHTML = display;
-      };
-      //Computes variables in display using the 'math' package
-      function equalCompute(){
-        if((display.slice(-1)).match(/[*\/\+-]/)){
-          display = display.slice(0,-1);
-        };
-        var x = math.eval(display);
-        document.getElementById('calc-window').innerHTML =  math.round(x,2);//*displays result
-        x = math.round(x,2);
+      }
+      // Computes variables in display using the 'math' package
+      function equalCompute() {
+        if ((display.slice(-1)).match(/[*\/\+-]/)) {
+          display = display.slice(0, -1);
+        }
+        let x = math.eval(display);
+        document.getElementById('calc-window').innerHTML =  math.round(x, 2); // *displays result
+        x = math.round(x, 2);
         display = x;
-      };
-      //Clears the window and sets the display variable to 0
-      function clearCompute(){
+      }
+      // Clears the window and sets the display variable to 0
+      function clearCompute() {
           document.getElementById('calc-window').innerHTML = '0';
           display = '0';
-      };
-      //Button click handlers
-      document.getElementById('one').onclick = function(){addToDisplay('1');};
-      document.getElementById('two').onclick = function(){addToDisplay('2');};
-      document.getElementById('three').onclick = function(){addToDisplay('3');};
-      document.getElementById('four').onclick = function(){addToDisplay('4');};
-      document.getElementById('five').onclick = function(){addToDisplay('5');};
-      document.getElementById('six').onclick = function(){addToDisplay('6');};
-      document.getElementById('seven').onclick = function(){addToDisplay('7');};
-      document.getElementById('eight').onclick = function(){addToDisplay('8');};
-      document.getElementById('nine').onclick = function(){addToDisplay('9');};
-      document.getElementById('zero').onclick = function(){addToDisplay('0');};
-      document.getElementById('decimal').onclick = function(){addToDisplay('.');};
-      document.getElementById('clear').onclick = function(){clearCompute();};
+      }
+      // Button click handlers
+      document.getElementById('one').onclick = function() {addToDisplay('1'); };
+      document.getElementById('two').onclick = function() {addToDisplay('2'); };
+      document.getElementById('three').onclick = function() {addToDisplay('3'); };
+      document.getElementById('four').onclick = function() {addToDisplay('4'); };
+      document.getElementById('five').onclick = function() {addToDisplay('5'); };
+      document.getElementById('six').onclick = function() {addToDisplay('6'); };
+      document.getElementById('seven').onclick = function() {addToDisplay('7'); };
+      document.getElementById('eight').onclick = function() {addToDisplay('8'); };
+      document.getElementById('nine').onclick = function() {addToDisplay('9'); };
+      document.getElementById('zero').onclick = function() {addToDisplay('0'); };
+      document.getElementById('decimal').onclick = function() {addToDisplay('.'); };
+      document.getElementById('clear').onclick = function() {clearCompute(); };
 
-      document.getElementById('multiplication').onclick = function(){addToDisplay('*');};
-      document.getElementById('divide').onclick = function(){addToDisplay('/');};
-      document.getElementById('plus').onclick = function(){addToDisplay('+');};
-      document.getElementById('minus').onclick = function(){addToDisplay('-');};
-      document.getElementById('equals').onclick = function(){equalCompute();};
+      document.getElementById('multiplication').onclick = function() {addToDisplay('*'); };
+      document.getElementById('divide').onclick = function() {addToDisplay('/'); };
+      document.getElementById('plus').onclick = function() {addToDisplay('+'); };
+      document.getElementById('minus').onclick = function() {addToDisplay('-'); };
+      document.getElementById('equals').onclick = function() {equalCompute(); };
 
-      document.getElementById('calc-window').innerHTML = display;//Initially sets window to display variable '0'
+      document.getElementById('calc-window').innerHTML = display; // Initially sets window to display variable '0'
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $:any
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -9,75 +9,75 @@ declare var $:any
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
+  currentVisible: string;
+  lastVisible: string;
+
+  constructor() {
 
   }
 
   ngOnInit() {
   }
 
-  currentVisible: string;
-  lastVisible: string;
-
-  descDisplay(x:string):void{
+  descDisplay(x: string): void {
     this.toggleCurrent(this.lastVisible);
     this.currentVisible = x;
     this.toggleCurrent(this.currentVisible);
     this.lastVisible = x;
   }
 
-  toggleCurrent(y:string):void{
-    switch(y){
-      case "#0":
+  toggleCurrent(y: string): void {
+    switch (y) {
+      case '#0':
         $('#0').toggleClass('desc-trans');
         break;
-      case "#1":
+      case '#1':
         $('#1').toggleClass('desc-trans');
         break;
-      case "#2":
+      case '#2':
         $('#2').toggleClass('desc-trans');
         break;
-      case "#3":
+      case '#3':
         $('#3').toggleClass('desc-trans');
         break;
-      case "#4":
+      case '#4':
         $('#4').toggleClass('desc-trans');
         break;
-      case "#5":
+      case '#5':
         $('#5').toggleClass('desc-trans');
         break;
-      
-      case "#7":
+
+      case '#7':
         $('#7').toggleClass('desc-trans');
         break;
-      case "#8":
+      case '#8':
         $('#8').toggleClass('desc-trans');
         break;
-      case "#9":
+      case '#9':
         $('#9').toggleClass('desc-trans');
         break;
-      case "#10":
+      case '#10':
         $('#10').toggleClass('desc-trans');
         break;
-      case "#11":
+      case '#11':
         $('#11').toggleClass('desc-trans');
         break;
-      case "#12":
+      case '#12':
         $('#12').toggleClass('desc-trans');
         break;
-      case "#13":
+      case '#13':
         $('#13').toggleClass('desc-trans');
         break;
-      case "#14":
+      case '#14':
         $('#14').toggleClass('desc-trans');
         break;
-      case "#15":
+      case '#15':
         $('#15').toggleClass('desc-trans');
         break;
-      case "#16":
+      case '#16':
         $('#16').toggleClass('desc-trans');
         break;
-      case "#17":
+      case '#17':
         $('#17').toggleClass('desc-trans');
         break;
       default:
